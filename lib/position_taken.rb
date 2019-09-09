f6bd42fa6959675477b6eb18fb9b4a9921e10add
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-09 14:49:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index == "X" || "O"
+  if board[index] == "X" || "O"
     return true
   else board[index] == " " || ""
     return false
